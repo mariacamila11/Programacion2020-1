ea67db0e8c5a3652c2ab45ecd4662a436c4dfdc4
mariacamila11 <herreram.maria@uces.edu.co>
2020-02-19 12:11:44 -0500

    hola mundo

---
+++ Clase a Clase/Clase1/Empresa.py
@@ -0,0 +1,9 @@
+#Codigo empresa 
+nombre=  "Hospital"
+print (nombre)
+empleados = 300
+print (empleados)
+gananciasAnuales = 232384294948942.3
+print (gananciasAnuales)
+
+
---
+++ Clase a Clase/Clase1/Hola mundo.py
@@ -0,0 +1,13 @@
+#Codigo primera clase
+nombre = "Camila"
+print (nombre)
+print (type(nombre))
+
+edad = 20
+print (edad)
+print (type(edad))
+
+dinero = 208435734858468848.8
+print (dinero)
+print (type(dinero))
+
